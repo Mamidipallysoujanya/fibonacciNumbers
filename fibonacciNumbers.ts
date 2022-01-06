bc4c0125a5ed4033c6ceb2d56c7ca811831b22a7
mamidipallysoujanya <mamidipallysoujanya93@gmail.com>
2022-01-06 15:40:47 +0530

    first 100 fibonacci numbers

---
+++ fibonacciNumbers.ts
@@ -0,0 +1,9 @@
+function fibonacci(num){
+    let fib=[0,1];
+    for(let i=2;i<num;i++){
+        fib[i]=fib[i-1]+fib[i-2];
+        fib.push(fib[i-2]+fib[i-1]);
+    }
+    return fib;
+}
+console.log(fibonacci(100));
